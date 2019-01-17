88e49536c163903daa5e7fd8866655bcd8063aea
Seth Karstad <skarstad@cs.ualberta.ca>
2019-01-16 19:56:27 -0700

    Commits by Seth Karstad

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,11 +1,15 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -18,12 +22,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class LonelyTwitterActivity extends Activity {
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	
+	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>(0);
+	private ArrayAdapter<Tweet> adapter;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -39,8 +48,17 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+
+				Tweet tweet = new ImportantTweet(text);
+
+				tweetList.add(tweet);
+
+				adapter.notifyDataSetChanged();
+
+
+				saveInFile();
+				//saveInFile(text, new Date(System.currentTimeMillis()));
+				//finish();
 
 			}
 		});
@@ -50,22 +68,37 @@ public class LonelyTwitterActivity extends Activity {
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+
+		loadFromFile();
+
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
 		oldTweetsList.setAdapter(adapter);
 	}
 
 	private String[] loadFromFile() {
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
-			FileInputStream fis = openFileInput(FILENAME);
+			/* FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
 			String line = in.readLine();
 			while (line != null) {
 				tweets.add(line);
 				line = in.readLine();
 			}
+			*/
+
+			FileReader in = new FileReader(new File(getFilesDir(), FILENAME));
+
+			Gson gson = new Gson();
+
+			// taken from https://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt Jan 19
+
+			Type type = new TypeToken<ArrayList<ImportantTweet>>(){}.getType();
+
+			tweetList = gson.fromJson(in, type);
+			in.close();
+
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -76,15 +109,22 @@ public class LonelyTwitterActivity extends Activity {
 		}
 		return tweets.toArray(new String[tweets.size()]);
 	}
-	
-	private void saveInFile(String text, Date date) {
+
+	private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
-			fos.close();
+			//FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_APPEND);
+			//fos.write(new String(date.toString() + " | " + text).getBytes());
+			//fos.close();
+
+			FileWriter out = new FileWriter(new File(getFilesDir(),FILENAME));
+
+			Gson gson = new Gson();
+
+			gson.toJson(tweetList, out);
+			out.close();
+
 		} catch (FileNotFoundException e) {
+
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/models/Mood.java
@@ -0,0 +1,21 @@
+package ca.ualberta.cs.lonelytwitter.models;
+
+import java.util.Date;
+
+public abstract class Mood {
+
+
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date new_date) {
+        date = new_date;
+    }
+
+
+
+
+}
